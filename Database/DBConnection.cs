c44f493971357b147c029a35bf152134e71bc421
diaz_daren3699-binary <darendiaz1214@gmail.com>
2026-05-31 21:26:56 +0800

    Update connection to MySQL, added App.config to the project and insert it to .gitignore.

--- Database/DBConnection.cs
+++ Database/DBConnection.cs
@@ -1,17 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Configuration;
 using MySql.Data.MySqlClient;
 
 namespace FinalsHRApplicantProcessWindowsApplication.Database
 {
     public class DBConnection
     {
-        private static string connectionString = "Server=localhost;Database=hr_applicant_db;Uid=root;Pwd=Binary_3699;";
-
         public static MySqlConnection GetConnection()
         {
-            return new MySqlConnection(connectionString);
+            string connStr = ConfigurationManager.ConnectionStrings["HRAppDB"].ConnectionString;
+            return new MySqlConnection(connStr);
         }
     }
 }   
