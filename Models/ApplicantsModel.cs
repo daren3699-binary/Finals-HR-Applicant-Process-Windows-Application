e02732eb507b90be9ac8d6aa4b12a789ce31f3b0
znvr <revenesbernardo1@gmail.com>
2026-06-05 01:48:44 +0800

    git add Models/ApplicantsModel.cs
    git commit -m "MODELS : Added ApplicantsModel class"

---
+++ Models/ApplicantsModel.cs
@@ -0,0 +1,12 @@
+﻿namespace FinalsHRApplicantProcessWindowsApplication.Models
+{
+    public class ApplicantsModel
+    {
+        public int ApplicantID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string ContactNumber { get; set; }
+        public string Address { get; set; }
+    }
+}
