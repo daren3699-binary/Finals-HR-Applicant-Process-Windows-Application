e2c5cc3dece333a0ef57bdd8bfcecb33637b739d
ayenestuaria-star <ayenestuaria@gmail.com>
2026-06-05 00:20:22 +0800

    FORM: Add JobVacancies.cs skeleton code

--- Forms/Applicant/JobVacancies.cs
+++ Forms/Applicant/JobVacancies.cs
@@ -16,3 +16,34 @@ namespace FinalsHRApplicantProcessWindowsApplication.Forms.Applicant
         }
     }
 }
+
+
+using System;
+using System.Windows.Forms;
+using FinalsHRApplicantProcessWindowsApplication.Database;
+using MySql.Data.MySqlClient;
+
+namespace FinalsHRApplicantProcessWindowsApplication.Forms.Applicant
+{
+    public partial class ApplicationStatus : Form
+    {
+        private int _applicantID;
+
+        public ApplicationStatus(int applicantID)
+        {
+            InitializeComponent();
+            _applicantID = applicantID;
+        }
+
+        private void ApplicationStatus_Load(object sender, EventArgs e)
+        {
+            LoadStatusHistory();
+        }
+
+        private void LoadStatusHistory()
+        {
+            // TODO: Load ApplicationStatusHistory records ordered by ChangedAt ASC
+            // Display as a timeline: status, date, remarks
+        }
+    }
+}
