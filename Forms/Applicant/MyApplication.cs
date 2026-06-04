abcf4a609aae44cdfc74e884279c89692eb8d2e9
sxdlyfe <ivanlouise1234@gmail.com>
2026-06-04 09:21:21 +0800

    FORM: Added necessary Windows Forms/Pages (My Profile, Job Vacancies, My Application, My Documents, and Application Status)

---
+++ Forms/Applicant/MyApplication.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FinalsHRApplicantProcessWindowsApplication.Forms.Applicant
+{
+    public partial class MyApplication : Form
+    {
+        public MyApplication()
+        {
+            InitializeComponent();
+        }
+    }
+}
