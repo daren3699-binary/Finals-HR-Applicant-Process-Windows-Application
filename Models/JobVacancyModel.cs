d31cd5198fa7387e2a7a8f8c4ecdb35dde9d3a4d
znvr <revenesbernardo1@gmail.com>
2026-06-05 01:53:15 +0800

    MODELS : Added JobVacancyModel class

---
+++ Models/JobVacancyModel.cs
@@ -0,0 +1,13 @@
+﻿namespace FinalsHRApplicantProcessWindowsApplication.Models
+{
+    public class JobVacancyModel
+    {
+        public int JobVacancyID { get; set; }
+        public string JobTitle { get; set; }
+        public int DepartmentID { get; set; }
+        public string Description { get; set; }
+        public string Qualifications { get; set; }
+        public string EmploymentType { get; set; }
+        public string Status { get; set; }
+    }
+}
