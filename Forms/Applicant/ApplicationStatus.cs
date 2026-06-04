5c26905ac73aff6b2b944f06cbd4565720447076
ayenestuaria-star <ayenestuaria@gmail.com>
2026-06-05 00:33:46 +0800

    FORM: Add ApplicationStatus.cs skeleton code

--- Forms/Applicant/ApplicationStatus.cs
+++ Forms/Applicant/ApplicationStatus.cs
@@ -1,18 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
 using System.Windows.Forms;
+using FinalsHRApplicantProcessWindowsApplication.Database;
+using MySql.Data.MySqlClient;
 
 namespace FinalsHRApplicantProcessWindowsApplication.Forms.Applicant
 {
     public partial class ApplicationStatus : Form
     {
-        public ApplicationStatus()
+        private int _applicantID;
+
+        public ApplicationStatus(int applicantID)
         {
             InitializeComponent();
+            _applicantID = applicantID;
+        }
+
+        private void ApplicationStatus_Load(object sender, EventArgs e)
+        {
+            LoadStatusHistory();
+        }
+
+        private void LoadStatusHistory()
+        {
+            // TODO: Load ApplicationStatusHistory records ordered by ChangedAt ASC
+            // Display as a timeline: status, date, remarks
         }
     }
 }
