952836d1f3229008b2baca6698ab78a4f42ef804
diaz_daren3699-binary <darendiaz1214@gmail.com>
2026-06-07 21:39:20 +0800

    FORM: Added InterviewSchedule, InterviewEvaluation, HiringDecision, designs

---
+++ Forms/HR/HiringDecision.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FinalsHRApplicantProcessWindowsApplication.Forms.HR
+{
+    public partial class HiringDecision : Form
+    {
+        public HiringDecision()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Forms/HR/InterviewEvaluation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FinalsHRApplicantProcessWindowsApplication.Forms.HR
+{
+    public partial class InterviewEvaluation : Form
+    {
+        public InterviewEvaluation()
+        {
+            InitializeComponent();
+        }
+    }
+}
