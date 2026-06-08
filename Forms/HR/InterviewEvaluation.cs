f3de949cba9f92a6c3f2e364d79dbb35b794dc23
LanceGomez <reenaurgmz@gmail.com>
2026-06-08 13:29:26 +0800

    FORM: Added InterviewEvaluation form skeleton
    Added constructor to initialize interviewID and updated form methods with TODO comments for future implementation.

--- Forms/HR/InterviewEvaluation.cs
+++ Forms/HR/InterviewEvaluation.cs
@@ -1,18 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
+using System;
 using System.Windows.Forms;
+using FinalsHRApplicantProcessWindowsApplication.Database;
+using FinalsHRApplicantProcessWindowsApplication.Helpers;
+using MySql.Data.MySqlClient;
 
 namespace FinalsHRApplicantProcessWindowsApplication.Forms.HR
 {
     public partial class InterviewEvaluation : Form
     {
-        public InterviewEvaluation()
+        private int _interviewID;
+
+        public InterviewEvaluation(int interviewID)
         {
             InitializeComponent();
+            _interviewID = interviewID;
+        }
+
+        private void InterviewEvaluation_Load(object sender, EventArgs e)
+        {
+            // TODO: Load interview details for reference
+            // TODO: Display applicant and interview information
+        }
+
+        private void btnSaveEvaluation_Click(object sender, EventArgs e)
+        {
+            // TODO: Validate evaluation inputs and score
+            // TODO: Insert evaluation record into InterviewEvaluations table
+            // TODO: Update interview status to 'Completed'
+            // TODO: Update application status to 'For Final Review'
+            // TODO: Log status change into ApplicationStatusHistory
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            // TODO: Close form without saving evaluation
         }
     }
 }
