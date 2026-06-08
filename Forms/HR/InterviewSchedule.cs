d6cebc91d81f9207c55c40949e34b44083e5fb03
LanceGomez <reenaurgmz@gmail.com>
2026-06-08 13:25:35 +0800

    FORM: Added InterviewSchedule form skeleton

--- Forms/HR/InterviewSchedule.cs
+++ Forms/HR/InterviewSchedule.cs
@@ -1,23 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
+using System;
 using System.Windows.Forms;
+using FinalsHRApplicantProcessWindowsApplication.Database;
+using FinalsHRApplicantProcessWindowsApplication.Helpers;
+using MySql.Data.MySqlClient;
 
 namespace FinalsHRApplicantProcessWindowsApplication.Forms.HR
 {
     public partial class InterviewSchedule : Form
     {
-        public InterviewSchedule()
+        private int _applicationID;
+
+        public InterviewSchedule(int applicationID)
         {
             InitializeComponent();
+            _applicationID = applicationID;
         }
 
         private void InterviewSchedule_Load(object sender, EventArgs e)
         {
+            // TODO: Load applicant information
+            // TODO: Populate available interviewers
+        }
 
+        private void btnSchedule_Click(object sender, EventArgs e)
+        {
+            // TODO: Validate interview schedule inputs
+            // TODO: Insert interview record into Interviews table
+            // TODO: Update application status to 'Interview Scheduled'
+            // TODO: Log status change into ApplicationStatusHistory
+            // TODO: Notify applicant about interview schedule
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            // TODO: Close form without saving changes
         }
     }
 }
